e6fbec83b4bbdc9d6839be30339447137071af10
Redo Martinez <adrian.redo-martinez@capgemini.com>
2018-01-25 09:36:34 +0100

    variable name correction

--- src/merger.ts
+++ src/merger.ts
@@ -10,17 +10,17 @@ export class MergerImpl implements Merger {
         for (const node of index[1]){
             if (node.kind === 'asciidoc' || node.kind === 'jira') {
                 if (textinSources[node.key]) {
-                    textinSources[node.key].getTranscript(node.id).then((data) => {
+                    textinSources[node.key].getTranscript(node.index).then((data) => {
                         transcripts.push(data); // The bind is by key -> A source identifier. The call to the method is customized for every node.
                     });
                 }
                 else {
                     // There isn't source-node binding
-                    const error_msg = 'Node with id \'' + node.id + '\' doesn\'t have an existing source';
+                    const error_msg = 'Node with id \'' + node.index + '\' doesn\'t have an existing source';
                     throw new Error(error_msg);
                 }
             } else {
-                const error_msg = '\'' + node.kind + '\'' + ' for \'' + node.id + '\' is an unknown TextInSource';
+                const error_msg = '\'' + node.kind + '\'' + ' for \'' + node.index + '\' is an unknown TextInSource';
                 throw new Error(error_msg);
             }
         }
